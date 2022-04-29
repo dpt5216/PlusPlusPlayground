a773e3f4fe8d20f8d0e45fc6d779b79264a2ed63
Dean Taipale <dpt5216@psu.edu>
2022-04-28 18:00:43 -0700

    file IO experiments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, char* argv[]){
-    std::cout << "Hello world\n";
+
+using namespace std;
+
+int main(int argc, char** argv){
+    cout << "Hello world\n";
+
+    ifstream dataIn ("data/datum1.txt"); // open file on initialization
+    string dataString; // gonna have to extrac
+
+
+    // *** read and print a file ***
+
+    while (dataIn.good()){
+        dataString = dataIn.get(); // pull out string without destroying whitespace
+        cout << dataString;
+    }
+    dataIn.close();
+
+
+    // *** increment a counter file ***
+
+    dataIn.open("data/datum2.txt");
+    dataIn >> dataString;
+    dataIn.close();
+
+    int runCount = stoi(dataString);
+    runCount ++;
+
+    ofstream dataOut ("data/datum2.txt");
+    dataOut << runCount;
+
 }
